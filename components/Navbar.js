0d27e7dad23ea4b45820c4a3f1a5c603b95ada38
M Aun Rasheed <greataun786@gmail.com>
2026-04-30 21:12:53 +0500

    feat: navoptin animated

--- components/Navbar.js
+++ components/Navbar.js
@@ -30,15 +30,15 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className='w-10/12 mx-auto flex items-center justify-between py-7 px-3'>
+            <nav className='w-10/12 mx-auto flex items-center justify-between py-4 px-3'>
                 <Link href={'/'}>
                     <h1 className='font-display font-bold text-4xl'>Odour</h1>
                 </Link>
-                <ul className='flex items-center gap-15'>
+                <ul className='flex items-center gap-3'>
                     {navOptions.map((opt, i) => (
-                        <li key={i} className='text-lg hover:text-muted transition-all ease-linear duration-300 relative group/navOption overflow-hidden'>
-                            <Link href={opt.src}>{opt.option}</Link>
-                            <span className='absolute bg-foreground text-background px-5 py-1 rounded-3xl -left-5 -bottom-10 transition-all ease-linear duration-300 group-hover/navOption:bottom-0'>{opt.option}</span>
+                        <li key={i} className='text-lg w-30 h-14 flex items-center justify-center transition-all ease-linear duration-300 relative group/navOption overflow-hidden'>
+                            <Link href={opt.src} className='group-hover/navOption:translate-y-10 group-hover/navOption:scale-0 transition-all ease-linear duration-300'>{opt.option}</Link>
+                            <span className='absolute bg-foreground text-background w-25 h-10 flex items-center justify-center rounded-3xl transition-all ease-linear duration-300 translate-y-12 scale-0 group-hover/navOption:scale-100 group-hover/navOption:translate-y-0 cursor-pointer'>{opt.option}</span>
                         </li>
                     ))}
                 </ul>
@@ -58,16 +58,17 @@ const Navbar = () => {
             <nav className={`
                 fixed top-0 left-0 w-full z-50
                 bg-background transition-transform duration-500 ease-in-out border-b border-foreground
-                ${fixNavbar ? 'translate-y-0' : '-translate-y-full'}
+                ${fixNavbar ? 'translate-y-0 scale-100' : '-translate-y-full scale-0'}
             `}>
-                <div className='w-10/12 mx-auto flex items-center justify-between py-7 px-3'>
+                <div className='w-10/12 mx-auto flex items-center justify-between py-4 px-3'>
                     <Link href={'/'}>
                         <h1 className='font-display font-bold text-4xl'>Odour</h1>
                     </Link>
-                    <ul className='flex items-center gap-15'>
+                    <ul className='flex items-center gap-3'>
                         {navOptions.map((opt, i) => (
-                            <li key={i} className='text-lg hover:text-muted transition-all ease-linear duration-300'>
-                                <Link href={opt.src}>{opt.option}</Link>
+                            <li key={i} className='text-lg w-30 h-14 flex items-center justify-center transition-all ease-linear duration-300 relative group/navOption overflow-hidden'>
+                                <Link href={opt.src} className='group-hover/navOption:translate-y-10 group-hover/navOption:scale-0 transition-all ease-linear duration-300'>{opt.option}</Link>
+                                <span className='absolute bg-foreground text-background w-25 h-10 flex items-center justify-center rounded-3xl transition-all ease-linear duration-300 translate-y-12 scale-0 group-hover/navOption:scale-100 group-hover/navOption:translate-y-0 cursor-pointer'>{opt.option}</span>
                             </li>
                         ))}
                     </ul>
