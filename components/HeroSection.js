c614b43ca20aa4d8090738116349618636c8c0ff
M Aun Rasheed <greataun786@gmail.com>
2026-05-02 12:08:55 +0500

    feat: hero section made responsive

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import { ChevronLeft, ChevronRight, ChevronUp, Dot } from 'lucide-react'
+import { ChevronLeft, ChevronRight, ChevronUp, Diamond, Dot } from 'lucide-react'
 import Image from 'next/image'
 import { motion } from "framer-motion"
 import { heroSectionSlideData } from './Assets'
@@ -59,9 +59,11 @@ const HeroSection = () => {
     <>
       <main className='bg-surface group relative'>
 
-        <div className='flex items-center justify-center absolute bottom-0 left-1/2 -translate-x-1/2 py-5'>
+        <div className='flex items-center justify-center gap-2 absolute bottom-0 left-1/2 -translate-x-1/2 py-5'>
           {heroSectionSlideData.map((slide, index) => {
-            return <span key={index} className={`${index === imgCount ? 'text-muted' : 'text-foreground'}`}><Dot size={60} /></span>
+            return <span key={index} className={`${index === imgCount ? 'text-muted' : 'text-foreground transform rotate-45'} transition-all ease-linear duration-500`}>
+              <Diamond strokeWidth={3} size={14}/>
+            </span>
           })}
         </div>
 
@@ -78,7 +80,7 @@ const HeroSection = () => {
           </svg>
         </a>
 
-        <section className='min-h-screen py-10 h-fit w-10/12 mx-auto flex items-center justify-between max-lg:flex-col'>
+        <section className='min-h-screen py-10 h-fit w-10/12 mx-auto flex items-center justify-between max-lg:flex-col max-lg:justify-center gap-10'>
 
           <section className='flex items-center gap-5 relative'>
 
@@ -136,9 +138,10 @@ const HeroSection = () => {
             <motion.div
               whileTap={{ scale: 0.95 }}
               onClick={handleForwardCarousel}
-              className='max-md:hidden opacity-0 group-hover:opacity-100 absolute -right-25 bg-background p-3 rounded-full border-2 border-foreground/20 hover:bg-foreground hover:border-surface hover:text-background transition-all ease-linear duration-300 cursor-pointer'>
+              className='opacity-0 group-hover:opacity-100 absolute -right-25 bg-background p-3 rounded-full border-2 border-foreground/20 hover:bg-foreground hover:border-surface hover:text-background transition-all ease-linear duration-300 cursor-pointer max-md:hidden'>
               <ChevronRight />
             </motion.div>
+
           </section>
         </section>
       </main>
