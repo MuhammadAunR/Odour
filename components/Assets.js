98187ae0327c8a8892e20efba7c8af02e486636c
M Aun Rasheed <greataun786@gmail.com>
2026-05-03 14:41:50 +0500

    feat: style updated of CARD

--- components/Assets.js
+++ components/Assets.js
@@ -43,67 +43,37 @@ const heroSectionSlideData = [
 export { heroSectionSlideData }
 
 const products = [
-    { id: 1, imgSrc: '/perfume-001.webp', alt: 'Sauvage by Dior', name: 'Sauvage', brand: 'Dior', category: 'Men', size: '100ml', originalPrice: 15000, price: 12500, isOnSale: true, discountPercent: 17, discountAmount: 2500, finalPrice: 12500 },
-
-    { id: 2, imgSrc: '/perfume-002.webp', alt: 'Bleu de Chanel', name: 'Bleu de Chanel', brand: 'Chanel', category: 'Men', size: '100ml', originalPrice: 18000, price: 18000, isOnSale: false, discountPercent: 0, discountAmount: 0, finalPrice: 18000 },
-
-    { id: 3, imgSrc: '/perfume-003.webp', alt: 'Black Opium by YSL', name: 'Black Opium', brand: 'Yves Saint Laurent', category: 'Women', size: '90ml', originalPrice: 14000, price: 9500, isOnSale: true, discountPercent: 32, discountAmount: 4500, finalPrice: 9500 },
-
-    { id: 4, imgSrc: '/perfume-004.webp', alt: 'La Vie Est Belle by Lancôme', name: 'La Vie Est Belle', brand: 'Lancôme', category: 'Women', size: '75ml', originalPrice: 13500, price: 11000, isOnSale: true, discountPercent: 19, discountAmount: 2500, finalPrice: 11000 },
-
-    { id: 5, imgSrc: '/perfume-005.webp', alt: 'Acqua di Gio by Giorgio Armani', name: 'Acqua di Giò', brand: 'Giorgio Armani', category: 'Men', size: '100ml', originalPrice: 13000, price: 13000, isOnSale: false, discountPercent: 0, discountAmount: 0, finalPrice: 13000 },
-
-    { id: 6, imgSrc: '/perfume-006.webp', alt: 'Chance Eau Tendre by Chanel', name: 'Chance Eau Tendre', brand: 'Chanel', category: 'Women', size: '50ml', originalPrice: 16000, price: 8500, isOnSale: true, discountPercent: 47, discountAmount: 7500, finalPrice: 8500 },
-
-    { id: 7, imgSrc: '/perfume-007.webp', alt: 'Oud Wood by Tom Ford', name: 'Oud Wood', brand: 'Tom Ford', category: 'Unisex', size: '50ml', originalPrice: 32000, price: 32000, isOnSale: false, discountPercent: 0, discountAmount: 0, finalPrice: 32000 },
-
-    { id: 8, imgSrc: '/perfume-008.webp', alt: 'Flowerbomb by Viktor & Rolf', name: 'Flowerbomb', brand: 'Viktor & Rolf', category: 'Women', size: '50ml', originalPrice: 12000, price: 7800, isOnSale: true, discountPercent: 35, discountAmount: 4200, finalPrice: 7800 },
-
-    { id: 9, imgSrc: '/perfume-009.webp', alt: 'Aventus by Creed', name: 'Aventus', brand: 'Creed', category: 'Men', size: '100ml', originalPrice: 45000, price: 45000, isOnSale: false, discountPercent: 0, discountAmount: 0, finalPrice: 45000 },
-
-    { id: 10, imgSrc: '/perfume-010.webp', alt: 'Libre by YSL', name: 'Libre', brand: 'Yves Saint Laurent', category: 'Women', size: '90ml', originalPrice: 14500, price: 10500, isOnSale: true, discountPercent: 28, discountAmount: 4000, finalPrice: 10500 },
-
-    { id: 11, imgSrc: '/perfume-011.webp', alt: 'Invictus by Paco Rabanne', name: 'Invictus', brand: 'Paco Rabanne', category: 'Men', size: '100ml', originalPrice: 12000, price: 9500, isOnSale: true, discountPercent: 21, discountAmount: 2500, finalPrice: 9500 },
-
-    { id: 12, imgSrc: '/perfume-012.webp', alt: 'Good Girl by Carolina Herrera', name: 'Good Girl', brand: 'Carolina Herrera', category: 'Women', size: '80ml', originalPrice: 15000, price: 12000, isOnSale: true, discountPercent: 20, discountAmount: 3000, finalPrice: 12000 },
-
-    { id: 13, imgSrc: '/perfume-013.webp', alt: '1 Million by Paco Rabanne', name: '1 Million', brand: 'Paco Rabanne', category: 'Men', size: '100ml', originalPrice: 14000, price: 14000, isOnSale: false, discountPercent: 0, discountAmount: 0, finalPrice: 14000 },
-
-    { id: 14, imgSrc: '/perfume-014.webp', alt: 'Daisy by Marc Jacobs', name: 'Daisy', brand: 'Marc Jacobs', category: 'Women', size: '75ml', originalPrice: 11000, price: 8500, isOnSale: true, discountPercent: 23, discountAmount: 2500, finalPrice: 8500 },
-
-    { id: 15, imgSrc: '/perfume-015.webp', alt: 'Terre d’Hermès', name: 'Terre d’Hermès', brand: 'Hermès', category: 'Men', size: '100ml', originalPrice: 17000, price: 17000, isOnSale: false, discountPercent: 0, discountAmount: 0, finalPrice: 17000 },
-
-    { id: 16, imgSrc: '/perfume-016.webp', alt: 'J’adore by Dior', name: 'J’adore', brand: 'Dior', category: 'Women', size: '90ml', originalPrice: 15500, price: 12500, isOnSale: true, discountPercent: 19, discountAmount: 3000, finalPrice: 12500 },
-
-    { id: 17, imgSrc: '/perfume-017.webp', alt: 'Eros by Versace', name: 'Eros', brand: 'Versace', category: 'Men', size: '100ml', originalPrice: 13000, price: 10000, isOnSale: true, discountPercent: 23, discountAmount: 3000, finalPrice: 10000 },
-
-    { id: 18, imgSrc: '/perfume-018.webp', alt: 'Bright Crystal by Versace', name: 'Bright Crystal', brand: 'Versace', category: 'Women', size: '90ml', originalPrice: 12000, price: 9000, isOnSale: true, discountPercent: 25, discountAmount: 3000, finalPrice: 9000 },
-
-    { id: 19, imgSrc: '/perfume-019.webp', alt: 'Spicebomb by Viktor & Rolf', name: 'Spicebomb', brand: 'Viktor & Rolf', category: 'Men', size: '90ml', originalPrice: 15000, price: 15000, isOnSale: false, discountPercent: 0, discountAmount: 0, finalPrice: 15000 },
-
-    { id: 20, imgSrc: '/perfume-020.webp', alt: 'Alien by Mugler', name: 'Alien', brand: 'Mugler', category: 'Women', size: '60ml', originalPrice: 14000, price: 11000, isOnSale: true, discountPercent: 21, discountAmount: 3000, finalPrice: 11000 },
-
-    { id: 21, imgSrc: '/perfume-021.webp', alt: 'Hugo Boss Bottled', name: 'Boss Bottled', brand: 'Hugo Boss', category: 'Men', size: '100ml', originalPrice: 12000, price: 9000, isOnSale: true, discountPercent: 25, discountAmount: 3000, finalPrice: 9000 },
-
-    { id: 22, imgSrc: '/perfume-022.webp', alt: 'Miss Dior', name: 'Miss Dior', brand: 'Dior', category: 'Women', size: '80ml', originalPrice: 15000, price: 12000, isOnSale: true, discountPercent: 20, discountAmount: 3000, finalPrice: 12000 },
-
-    { id: 23, imgSrc: '/perfume-023.webp', alt: 'Nautica Voyage', name: 'Voyage', brand: 'Nautica', category: 'Men', size: '100ml', originalPrice: 7000, price: 5500, isOnSale: true, discountPercent: 21, discountAmount: 1500, finalPrice: 5500 },
-
-    { id: 24, imgSrc: '/perfume-024.webp', alt: 'Si by Armani', name: 'Si', brand: 'Giorgio Armani', category: 'Women', size: '100ml', originalPrice: 16000, price: 13000, isOnSale: true, discountPercent: 19, discountAmount: 3000, finalPrice: 13000 },
-
-    { id: 25, imgSrc: '/perfume-025.webp', alt: 'Legend by Montblanc', name: 'Legend', brand: 'Montblanc', category: 'Men', size: '100ml', originalPrice: 11000, price: 11000, isOnSale: false, discountPercent: 0, discountAmount: 0, finalPrice: 11000 },
-
-    { id: 26, imgSrc: '/perfume-026.webp', alt: 'Bombshell by Victoria’s Secret', name: 'Bombshell', brand: 'Victoria’s Secret', category: 'Women', size: '100ml', originalPrice: 10000, price: 8000, isOnSale: true, discountPercent: 20, discountAmount: 2000, finalPrice: 8000 },
-
-    { id: 27, imgSrc: '/perfume-027.webp', alt: 'Black XS by Paco Rabanne', name: 'Black XS', brand: 'Paco Rabanne', category: 'Men', size: '100ml', originalPrice: 12500, price: 12500, isOnSale: false, discountPercent: 0, discountAmount: 0, finalPrice: 12500 },
-
-    { id: 28, imgSrc: '/perfume-028.webp', alt: 'Coco Mademoiselle', name: 'Coco Mademoiselle', brand: 'Chanel', category: 'Women', size: '100ml', originalPrice: 18000, price: 15000, isOnSale: true, discountPercent: 17, discountAmount: 3000, finalPrice: 15000 },
-
-    { id: 29, imgSrc: '/perfume-029.webp', alt: 'Ultra Male by Jean Paul Gaultier', name: 'Ultra Male', brand: 'Jean Paul Gaultier', category: 'Men', size: '75ml', originalPrice: 14000, price: 11000, isOnSale: true, discountPercent: 21, discountAmount: 3000, finalPrice: 11000 },
-
-    { id: 30, imgSrc: '/perfume-030.webp', alt: 'Scandal by Jean Paul Gaultier', name: 'Scandal', brand: 'Jean Paul Gaultier', category: 'Women', size: '80ml', originalPrice: 14500, price: 11500, isOnSale: true, discountPercent: 21, discountAmount: 3000, finalPrice: 11500 },
-
-    { id: 31, imgSrc: '/perfume-031.webp', alt: '212 VIP by Carolina Herrera', name: '212 VIP', brand: 'Carolina Herrera', category: 'Unisex', size: '100ml', originalPrice: 16000, price: 13500, isOnSale: true, discountPercent: 16, discountAmount: 2500, finalPrice: 13500 },
+    { id: 1,  imgSrc: '/perfume-001.webp', alt: 'Sauvage by Dior',                name: 'Sauvage',           brand: 'Dior',               category: 'Men',    size: '100ml', price: 15000, discountedPrice: 12500, isOnSale: true  },
+    { id: 2,  imgSrc: '/perfume-002.webp', alt: 'Bleu de Chanel',                 name: 'Bleu de Chanel',    brand: 'Chanel',             category: 'Men',    size: '100ml', price: 18000, discountedPrice: null,  isOnSale: false },
+    { id: 3,  imgSrc: '/perfume-003.webp', alt: 'Black Opium by YSL',             name: 'Black Opium',       brand: 'Yves Saint Laurent', category: 'Women',  size: '90ml',  price: 14000, discountedPrice: 9500,  isOnSale: true  },
+    { id: 4,  imgSrc: '/perfume-004.webp', alt: 'La Vie Est Belle by Lancôme',    name: 'La Vie Est Belle',  brand: 'Lancôme',            category: 'Women',  size: '75ml',  price: 13500, discountedPrice: 11000, isOnSale: true  },
+    { id: 5,  imgSrc: '/perfume-005.webp', alt: 'Acqua di Gio by Giorgio Armani', name: 'Acqua di Giò',      brand: 'Giorgio Armani',     category: 'Men',    size: '100ml', price: 13000, discountedPrice: null,  isOnSale: false },
+    { id: 6,  imgSrc: '/perfume-006.webp', alt: 'Chance Eau Tendre by Chanel',    name: 'Chance Eau Tendre', brand: 'Chanel',             category: 'Women',  size: '50ml',  price: 16000, discountedPrice: 8500,  isOnSale: true  },
+    { id: 7,  imgSrc: '/perfume-007.webp', alt: 'Oud Wood by Tom Ford',           name: 'Oud Wood',          brand: 'Tom Ford',           category: 'Unisex', size: '50ml',  price: 32000, discountedPrice: null,  isOnSale: false },
+    { id: 8,  imgSrc: '/perfume-008.webp', alt: 'Flowerbomb by Viktor & Rolf',    name: 'Flowerbomb',        brand: 'Viktor & Rolf',      category: 'Women',  size: '50ml',  price: 12000, discountedPrice: 7800,  isOnSale: true  },
+    { id: 9,  imgSrc: '/perfume-009.webp', alt: 'Aventus by Creed',               name: 'Aventus',           brand: 'Creed',              category: 'Men',    size: '100ml', price: 45000, discountedPrice: null,  isOnSale: false },
+    { id: 10, imgSrc: '/perfume-010.webp', alt: 'Libre by YSL',                   name: 'Libre',             brand: 'Yves Saint Laurent', category: 'Women',  size: '90ml',  price: 14500, discountedPrice: 10500, isOnSale: true  },
+    { id: 11, imgSrc: '/perfume-011.webp', alt: 'Invictus by Paco Rabanne',       name: 'Invictus',          brand: 'Paco Rabanne',       category: 'Men',    size: '100ml', price: 12000, discountedPrice: 9500,  isOnSale: true  },
+    { id: 12, imgSrc: '/perfume-012.webp', alt: 'Good Girl by Carolina Herrera',  name: 'Good Girl',         brand: 'Carolina Herrera',   category: 'Women',  size: '80ml',  price: 15000, discountedPrice: 12000, isOnSale: true  },
+    { id: 13, imgSrc: '/perfume-013.webp', alt: '1 Million by Paco Rabanne',      name: '1 Million',         brand: 'Paco Rabanne',       category: 'Men',    size: '100ml', price: 14000, discountedPrice: null,  isOnSale: false },
+    { id: 14, imgSrc: '/perfume-014.webp', alt: 'Daisy by Marc Jacobs',           name: 'Daisy',             brand: 'Marc Jacobs',        category: 'Women',  size: '75ml',  price: 11000, discountedPrice: 8500,  isOnSale: true  },
+    { id: 15, imgSrc: '/perfume-015.webp', alt: "Terre d'Hermès",                 name: "Terre d'Hermès",    brand: 'Hermès',             category: 'Men',    size: '100ml', price: 17000, discountedPrice: null,  isOnSale: false },
+    { id: 16, imgSrc: '/perfume-016.webp', alt: "J'adore by Dior",                name: "J'adore",           brand: 'Dior',               category: 'Women',  size: '90ml',  price: 15500, discountedPrice: 12500, isOnSale: true  },
+    { id: 17, imgSrc: '/perfume-017.webp', alt: 'Eros by Versace',                name: 'Eros',              brand: 'Versace',            category: 'Men',    size: '100ml', price: 13000, discountedPrice: 10000, isOnSale: true  },
+    { id: 18, imgSrc: '/perfume-018.webp', alt: 'Bright Crystal by Versace',      name: 'Bright Crystal',    brand: 'Versace',            category: 'Women',  size: '90ml',  price: 12000, discountedPrice: 9000,  isOnSale: true  },
+    { id: 19, imgSrc: '/perfume-019.webp', alt: 'Spicebomb by Viktor & Rolf',     name: 'Spicebomb',         brand: 'Viktor & Rolf',      category: 'Men',    size: '90ml',  price: 15000, discountedPrice: null,  isOnSale: false },
+    { id: 20, imgSrc: '/perfume-020.webp', alt: 'Alien by Mugler',                name: 'Alien',             brand: 'Mugler',             category: 'Women',  size: '60ml',  price: 14000, discountedPrice: 11000, isOnSale: true  },
+    { id: 21, imgSrc: '/perfume-021.webp', alt: 'Hugo Boss Bottled',              name: 'Boss Bottled',      brand: 'Hugo Boss',          category: 'Men',    size: '100ml', price: 12000, discountedPrice: 9000,  isOnSale: true  },
+    { id: 22, imgSrc: '/perfume-022.webp', alt: 'Miss Dior',                      name: 'Miss Dior',         brand: 'Dior',               category: 'Women',  size: '80ml',  price: 15000, discountedPrice: 12000, isOnSale: true  },
+    { id: 23, imgSrc: '/perfume-023.webp', alt: 'Nautica Voyage',                 name: 'Voyage',            brand: 'Nautica',            category: 'Men',    size: '100ml', price: 7000,  discountedPrice: 5500,  isOnSale: true  },
+    { id: 24, imgSrc: '/perfume-024.webp', alt: 'Si by Armani',                   name: 'Si',                brand: 'Giorgio Armani',     category: 'Women',  size: '100ml', price: 16000, discountedPrice: 13000, isOnSale: true  },
+    { id: 25, imgSrc: '/perfume-025.webp', alt: 'Legend by Montblanc',            name: 'Legend',            brand: 'Montblanc',          category: 'Men',    size: '100ml', price: 11000, discountedPrice: null,  isOnSale: false },
+    { id: 26, imgSrc: '/perfume-026.webp', alt: "Bombshell by Victoria's Secret", name: 'Bombshell',         brand: "Victoria's Secret",  category: 'Women',  size: '100ml', price: 10000, discountedPrice: 8000,  isOnSale: true  },
+    { id: 27, imgSrc: '/perfume-027.webp', alt: 'Black XS by Paco Rabanne',       name: 'Black XS',          brand: 'Paco Rabanne',       category: 'Men',    size: '100ml', price: 12500, discountedPrice: null,  isOnSale: false },
+    { id: 28, imgSrc: '/perfume-028.webp', alt: 'Coco Mademoiselle',              name: 'Coco Mademoiselle', brand: 'Chanel',             category: 'Women',  size: '100ml', price: 18000, discountedPrice: 15000, isOnSale: true  },
+    { id: 29, imgSrc: '/perfume-029.webp', alt: 'Ultra Male by JPG',              name: 'Ultra Male',        brand: 'Jean Paul Gaultier', category: 'Men',    size: '75ml',  price: 14000, discountedPrice: 11000, isOnSale: true  },
+    { id: 30, imgSrc: '/perfume-030.webp', alt: 'Scandal by JPG',                 name: 'Scandal',           brand: 'Jean Paul Gaultier', category: 'Women',  size: '80ml',  price: 14500, discountedPrice: 11500, isOnSale: true  },
+    { id: 31, imgSrc: '/perfume-031.webp', alt: '212 VIP by Carolina Herrera',    name: '212 VIP',           brand: 'Carolina Herrera',   category: 'Unisex', size: '100ml', price: 16000, discountedPrice: 13500, isOnSale: true  },
 ];
 
 export { products };
--- components/CardUI.js
+++ components/CardUI.js
@@ -1,19 +1,23 @@
 'use client'
-import { Search, ShoppingBag } from 'lucide-react'
+import { Search, ShoppingBag, Heart } from 'lucide-react'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from "framer-motion"
 import { useCart } from '@/app/context/CartContext'
+import { Button1 } from './ButtonUI'
 
 const ProductCard = ({ product }) => {
     const { handleAddCartItems } = useCart()
+    const [wishListed, setWishListed] = useState(false)
+
     return (
         <motion.div
             initial={{ opacity: 0, scale: 0.92 }}
             whileInView={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5, delay: product.id * 0.1 }}
             viewport={{ once: true }}
-            className='flex flex-col items-start justify-center border border-surface group/productCard hover:shadow-xl transition-shadow ease-in-out duration-500'>
+            className='flex flex-col items-start border border-surface group/productCard hover:shadow-xl transition-shadow ease-in-out duration-500'>
+
             <div className='relative w-full aspect-3/4 overflow-hidden'>
                 <Image
                     src={product.imgSrc}
@@ -21,26 +25,83 @@ const ProductCard = ({ product }) => {
                     fill
                     loading='lazy'
                     sizes='(max-width: 768px) 50vw, 25vw'
-                    className='object-cover'
+                    className='object-cover transition-transform duration-500 group-hover/productCard:scale-105'
                 />
-                <div className='flex items-center justify-center gap-5 absolute -bottom-15 left-1/2 -translate-x-1/2 scale-0 group-hover/productCard:bottom-2 group-hover/productCard:scale-100 transition-all ease-in-out duration-300 bg-white border border-foreground/20 px-7 py-2 rounded-full'>
-                    <span className='hover:text-muted cursor-pointer transition-colors ease-in-out relative group/productActions'>
-                        <Search />
-                        <span className='absolute top-0 scale-0 left-1/2 -translate-x-1/2 text-xs bg-foreground text-background w-20 flex items-center justify-center group-hover/productActions:-top-12 group-hover/productActions:scale-100 transition-all ease-linear rounded-full p-2 whitespace-nowrap'>Quick View</span>
+
+
+                <span className={`absolute top-2 left-2 px-2 py-1 text-xs font-medium
+                    ${product.category === 'Men'
+                        ? 'bg-foreground text-background'
+                        : product.category === 'Women'
+                            ? 'bg-foreground text-background'
+                            : 'bg-foreground text-background'
+                    }`}>
+                    {product.category}
+                </span>
+
+                {product.isOnSale &&
+                    <span className='absolute top-2 right-2 px-2 py-1 text-xs font-semibold bg-red-500 text-white'>
+                        -{Math.round(((product.price - product.discountedPrice) / product.price) * 100)}%
                     </span>
-                    <span onClick={() => handleAddCartItems(product)} className='hover:text-muted cursor-pointer transition-colors ease-in-out relative group/productActions'>
-                        <ShoppingBag />
-                        <span className='absolute top-0 scale-0 left-1/2 -translate-x-1/2 text-xs bg-foreground text-background w-20 flex items-center justify-center group-hover/productActions:-top-12 group-hover/productActions:scale-100 transition-all ease-linear rounded-full p-2 whitespace-nowrap'>Add to Cart</span>
-                    </span>
+                }
+
+
+                <button
+                    onClick={() => setWishListed(!wishListed)}
+                    className='absolute bottom-14 right-3 p-1.5 bg-background/80 backdrop-blur-sm rounded-full
+                        opacity-0 group-hover/productCard:opacity-100 transition-all duration-300'>
+                    <Heart
+                        size={16}
+                        className={wishListed ? 'fill-red-500 text-red-500' : 'text-foreground'}
+                    />
+                </button>
+
+
+                <div className='absolute bottom-0 left-0 right-0 
+                    translate-y-full group-hover/productCard:translate-y-0
+                    transition-transform duration-300 ease-in-out
+                    flex items-center border-t border-foreground/10 bg-background/95 backdrop-blur-sm'>
+
+                    <button
+                        className='flex-1 flex items-center justify-center gap-2 py-3 text-sm
+                            hover:bg-muted hover:text-background transition-colors duration-300
+                            border-r border-foreground/10'>
+                        <Search size={15} />
+                        <span>Quick View</span>
+                    </button>
+
+                    <button
+                        onClick={() => handleAddCartItems(product)}
+                        className='flex-1 flex items-center justify-center gap-2 py-3 text-sm
+                            hover:bg-muted hover:text-background transition-colors duration-300'>
+                        <ShoppingBag size={15} />
+                        <span>Add to Cart</span>
+                    </button>
+
                 </div>
             </div>
-            <div className='p-4'>
+
+
+            <div className='p-4 w-full'>
                 <h3 className='text-xl font-bold font-display'>{product.name}</h3>
-                <div className=''>
-                    <span className='text-xs'>PKR</span>
-                    <span className='font-semibold text-red-600'> {product.price}</span>
-                </div>
+                <p className='text-xs text-foreground/40 mb-1'>{product.brand}</p>
+
+                {product.isOnSale ? (
+                    <div className='flex items-baseline gap-2'>
+                        <span className='text-sm text-foreground/40 line-through'>
+                            PKR {product.price.toLocaleString()}
+                        </span>
+                        <span className='font-bold text-red-500'>
+                            PKR {product.discountedPrice.toLocaleString()}
+                        </span>
+                    </div>
+                ) : (
+                    <span className='font-bold text-foreground'>
+                        PKR {product.price.toLocaleString()}
+                    </span>
+                )}
             </div>
+
         </motion.div>
     )
 }
