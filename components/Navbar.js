a21021472c00c7968880b2c971e0e77f017f4e4a
M Aun Rasheed <greataun786@gmail.com>
2026-05-05 16:58:18 +0500

    styles: updated bg of navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -40,48 +40,51 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className='w-10/12 mx-auto flex items-center justify-between py-4 h-20'>
-                <Link href={'/'} className=''>
-                    <h1 className='font-display font-bold text-4xl max-lg:hidden'>Odour</h1>
-                    <span className='lg:hidden'>
-                        <HamburgerComp isOpen={isOpen} onClick={toggleNavbar} />
-                    </span>
-                </Link>
+            <nav className='bg-foreground/5'>
+                <section className='w-10/12 mx-auto flex items-center justify-between py-4 h-20'>
 
-                <h1 className='font-display font-bold text-4xl lg:hidden'>Odour</h1>
+                    <Link href={'/'} className=''>
+                        <h1 className='font-display font-bold text-4xl max-lg:hidden'>Odour</h1>
+                        <span className='lg:hidden'>
+                            <HamburgerComp isOpen={isOpen} onClick={toggleNavbar} />
+                        </span>
+                    </Link>
 
-                <ul className='flex items-center gap-3 max-lg:hidden'>
-                    {navOptions.map((opt, i) => (
-                        <Link onClick={() => handleActiveNavOption(opt.option)} href={opt.src} key={i} className='text-lg w-25 h-14 flex items-center justify-center transition-all ease-linear duration-300 relative group/navOption overflow-hidden'>
+                    <h1 className='font-display font-bold text-4xl lg:hidden'>Odour</h1>
 
-                            <li className={`transition-all ease-linear duration-300 ${activeNav === opt.option.toLowerCase() ? '' : 'group-hover/navOption:translate-y-10 group-hover/navOption:scale-0'}`}>{opt.option}</li>
-                            <span className={`absolute bg-foreground text-background w-20 px-2 h-10 flex items-center justify-center rounded-3xl transition-all ease-linear duration-300 cursor-pointer
+                    <ul className='flex items-center gap-3 max-lg:hidden'>
+                        {navOptions.map((opt, i) => (
+                            <Link onClick={() => handleActiveNavOption(opt.option)} href={opt.src} key={i} className='text-lg w-25 h-14 flex items-center justify-center transition-all ease-linear duration-300 relative group/navOption overflow-hidden'>
+
+                                <li className={`transition-all ease-linear duration-300 ${activeNav === opt.option.toLowerCase() ? '' : 'group-hover/navOption:translate-y-10 group-hover/navOption:scale-0'}`}>{opt.option}</li>
+                                <span className={`absolute bg-foreground text-background w-20 px-2 h-10 flex items-center justify-center rounded-3xl transition-all ease-linear duration-300 cursor-pointer
                             ${activeNav === opt.option.toLowerCase() ? 'translate-y-0  scale-100' : 'translate-y-12  scale-0 group-hover/navOption:scale-100 group-hover/navOption:translate-y-0'}`}>{opt.option}</span>
 
-                        </Link>
-                    ))}
-                </ul>
-                <div className='flex items-center gap-5'>
-                    <span className='flex items-center gap-2'>
-                        <span onClick={toggleCart} className='hover:text-muted transition-all ease-linear duration-300 cursor-pointer relative'>
-                            <ShoppingBag />
-                            {cartItems.length > 0 &&
-                                <span className='absolute w-5 h-5 rounded-full bg-foreground text-background text-xs -top-2 -left-2 flex items-center justify-center'>
-                                    {cartItems.length}
-                                </span>
-                            }
+                            </Link>
+                        ))}
+                    </ul>
+                    <div className='flex items-center gap-5'>
+                        <span className='flex items-center gap-2'>
+                            <span onClick={toggleCart} className='hover:text-muted transition-all ease-linear duration-300 cursor-pointer relative'>
+                                <ShoppingBag />
+                                {cartItems.length > 0 &&
+                                    <span className='absolute w-5 h-5 rounded-full bg-foreground text-background text-xs -top-2 -left-2 flex items-center justify-center'>
+                                        {cartItems.length}
+                                    </span>
+                                }
+                            </span>
+                            <span className='max-md:hidden'><span className='text-xs'>PKR</span> 0.00</span>
                         </span>
-                        <span className='max-md:hidden'><span className='text-xs'>PKR</span> 0.00</span>
-                    </span>
-                    <span className='hover:text-muted transition-all ease-linear duration-300 cursor-pointer'>
-                        <UserRound />
-                    </span>
-                </div>
+                        <span className='hover:text-muted transition-all ease-linear duration-300 cursor-pointer'>
+                            <UserRound />
+                        </span>
+                    </div>
+                </section>
             </nav>
 
             <nav className={`
                 fixed top-0 left-0 w-full z-50
-                bg-background transition-transform duration-500 ease-in-out border-b border-foreground
+                bg-background/50 backdrop-blur-2xl transition-transform duration-500 ease-in-out border-b border-foreground
                 ${fixNavbar ? 'translate-y-0 scale-100' : '-translate-y-full scale-0'}
             `}>
                 <div className='w-10/12 mx-auto flex items-center justify-between py-4 h-20'>
@@ -94,9 +97,12 @@ const Navbar = () => {
                     <h1 className='font-display font-bold text-4xl lg:hidden'>Odour</h1>
                     <ul className='flex items-center gap-3 max-lg:hidden'>
                         {navOptions.map((opt, i) => (
-                            <Link href={opt.src} key={i} className='text-lg w-25 h-14 flex items-center justify-center transition-all ease-linear duration-300 relative group/navOption overflow-hidden'>
-                                <li className='group-hover/navOption:translate-y-10 group-hover/navOption:scale-0 transition-all ease-linear duration-300'>{opt.option}</li>
-                                <span className='absolute bg-foreground text-background w-20 px-2 h-10 flex items-center justify-center rounded-3xl transition-all ease-linear duration-300 translate-y-12 scale-0 group-hover/navOption:scale-100 group-hover/navOption:translate-y-0 cursor-pointer'>{opt.option}</span>
+                            <Link onClick={() => handleActiveNavOption(opt.option)} href={opt.src} key={i} className='text-lg w-25 h-14 flex items-center justify-center transition-all ease-linear duration-300 relative group/navOption overflow-hidden'>
+                                <li className={`transition-all ease-linear duration-300
+                                     ${activeNav === opt.option.toLowerCase() ? '' : 'group-hover/navOption:translate-y-10 group-hover/navOption:scale-0'}`}>{opt.option}</li>
+                                <span className={`absolute bg-foreground text-background w-20 px-2 h-10 flex items-center justify-center rounded-3xl transition-all ease-linear duration-300 cursor-pointer
+                            ${activeNav === opt.option.toLowerCase() ? 'translate-y-0  scale-100' : 'translate-y-12  scale-0 group-hover/navOption:scale-100 group-hover/navOption:translate-y-0'}`}>{opt.option}</span>
+
                             </Link>
                         ))}
                     </ul>
