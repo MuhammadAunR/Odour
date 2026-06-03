48c382ec4ebac2dee164582d1c3e6fe7e1de7291
M Aun Rasheed <greataun786@gmail.com>
2026-06-03 06:21:07 -0700

    feat: added wishlist items to local storage

--- app/context/WishlistContext.js
+++ app/context/WishlistContext.js
@@ -1,40 +1,46 @@
-'use client'
+"use client";
 
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useEffect, useState } from "react";
 
-export const ContextProvider = createContext()
-export const useWishlist = () => useContext(ContextProvider)
+export const ContextProvider = createContext();
+export const useWishlist = () => useContext(ContextProvider);
 
-import React from 'react'
-import { toast } from "react-toastify"
+import React from "react";
+
+import { toast } from "react-toastify";
 
 const WishlistContext = ({ children }) => {
+  const [wishListItems, setWishListItems] = useState([]);
 
-    const [wishListItems, setWishListItems] = useState([])
+  useEffect(() => {
+    const stored = localStorage.getItem("wishListedProductIds");
+    if (stored) setWishListItems(JSON.parse(stored));
+  }, []);
 
-    const toggleWishList = (prodId) => {
-        const exist = wishListItems?.find(itemId => {
-            return itemId === prodId
-        })
-        if (exist) {
-            setWishListItems(prev => {
-                return prev.filter(id => id !== prodId)
-            })
-            toast.success('Removed from Wishlist')
-        } else {
-            setWishListItems(prev => (
-                [...prev, prodId]
-            ))
-            toast.success('Added to Wishlist')
-        }
-    }
-    return (
-        <>
-            <ContextProvider.Provider value={{ toggleWishList, wishListItems }}>
-                {children}
-            </ContextProvider.Provider>
-        </>
-    )
-}
+  useEffect(() => {
+    localStorage.setItem("wishListedProductIds", JSON.stringify(wishListItems));
+  }, [wishListItems]);
 
-export default WishlistContext
+  const toggleWishList = (prodId) => {
+    setWishListItems((prev) =>
+      prev.includes(prodId)
+        ? prev.filter((id) => id !== prodId)
+        : [...prev, prodId],
+    );
+
+    toast.success(
+      wishListItems.includes(prodId)
+        ? "Removed from Wishlist"
+        : "Added to Wishlist",
+    );
+  };
+  return (
+    <>
+      <ContextProvider.Provider value={{ toggleWishList, wishListItems }}>
+        {children}
+      </ContextProvider.Provider>
+    </>
+  );
+};
+
+export default WishlistContext;
