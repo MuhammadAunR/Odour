e34f10674775e53532730ef32f74203be1165a12
M Aun Rasheed <greataun786@gmail.com>
2026-07-19 11:29:48 +0500

    refactor: reduces toast's autoClose time to 2s

--- components/ReactToastContainer.js
+++ components/ReactToastContainer.js
@@ -6,7 +6,7 @@ const ReactToastContainer = ({ children }) => {
         <>
             <ToastContainer
                 position="top-center"
-                autoClose={3000}
+                autoClose={2000}
                 hideProgressBar={false}
                 newestOnTop={false}
                 closeOnClick={false}
