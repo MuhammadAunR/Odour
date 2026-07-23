de7718edb953ff4f20916a362d8360cca9795dc5
M Aun Rasheed <greataun786@gmail.com>
2026-07-23 12:18:37 +0500

    feat: added new fragranceFamilies and attributes

--- constants/ProductClassificationConstants.js
+++ constants/ProductClassificationConstants.js
@@ -1,7 +1,7 @@
 const gender = ['Men', 'Women', 'Unisex']
 const seasons = ['Spring', 'Winter', 'Summer', 'Fall']
-const fragranceFamily = ['Woody', 'Citrus', 'Floral', 'Fresh', 'Oriental']
+const fragranceFamily = ['Woody', 'Citrus', 'Floral', 'Fresh', 'Oriental', 'Leather', 'Sweet', 'Gaurmand', 'Spicy']
 const productCategories = ['Perfume', 'Attar', 'Tester', 'Body Spray', 'Deodorant']
-const productAttributes = ['Limited Edition', 'New Arrivals', 'Best Sellers', 'Trending']
+const productAttributes = ['Limited Edition', 'New Arrivals', 'Best Sellers', 'Trending', 'Casual']
 
 export { gender, seasons, fragranceFamily, productAttributes, productCategories }
