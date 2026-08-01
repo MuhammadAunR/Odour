6c78c146b491a5a678f7f4d9da61c7a0cac2c091
M Aun Rasheed <greataun786@gmail.com>
2026-08-01 12:14:43 +0500

    refactor: removed quick view import form main page.js

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -1,13 +1,11 @@
 import FeatureStrip from "@/components/main/FeatureStrip";
 import HeroSection from "@/components/main/HeroSection";
-import ProductQuickView from "@/components/main/ProductQuickView";
 import ProductSection from "@/components/main/ProductSection";
 
 
 export default function Home() {
   return (
     <>
-      <ProductQuickView />
       <header>
         <HeroSection />
       </header>
