6151cbb2446bf8eb55a3175e8c38e3b9a3680862
M Aun Rasheed <greataun786@gmail.com>
2026-08-05 08:41:36 +0500

    feat(api): add GET and POST endpoints for fragrance family

---
+++ app/api/catalog/fragrance-family/route.js
@@ -0,0 +1,60 @@
+import { connectDB } from "@/lib/mongodb";
+import { generateSLUG } from "@/lib/productUtils";
+import FragranceFamily from "@/models/FragranceFamily";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+    try {
+        await connectDB()
+
+        const { name, isActive } = await req.json()
+        const slug = generateSLUG(name)
+
+        const existing = await FragranceFamily.findOne({ name })
+        if (existing) {
+            return NextResponse.json(
+                { message: 'Fragrance family already exists', existing },
+                { status: 409 }
+            )
+        }
+
+        const data = await FragranceFamily.create({
+            name,
+            slug,
+            isActive,
+        })
+        return NextResponse.json(
+            { message: 'Fragrance family added successfully', data },
+            { status: 201 }
+        )
+    } catch (error) {
+        return NextResponse.json(
+            { message: 'Failed to add Fragrance family', error: error.message },
+            { status: 400 }
+        )
+    }
+}
+
+export async function GET() {
+    try {
+        await connectDB()
+        const data = await FragranceFamily.find()
+
+        if (data.length === 0) {
+            return NextResponse.json(
+                { message: 'Fragrance familes not found', data },
+                { status: 404 }
+            )
+        }
+
+        return NextResponse.json(
+            { message: 'Successfully fetched Fragrance families', data },
+            { status: 200 }
+        )
+    } catch (error) {
+        return NextResponse.json(
+            { message: 'Failed to fetch Fragrance families', error: error.message },
+            { status: 400 }
+        )
+    }
+}
