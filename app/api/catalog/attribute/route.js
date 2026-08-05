60b2027710c2a73248844c2ce9f5583010599efd
M Aun Rasheed <greataun786@gmail.com>
2026-08-05 08:43:47 +0500

    feat(api): add GET and POST endpoints for attribute

---
+++ app/api/catalog/attribute/route.js
@@ -0,0 +1,60 @@
+import { connectDB } from "@/lib/mongodb";
+import { generateSLUG } from "@/lib/productUtils";
+import Attribute from "@/models/Attribute";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+    try {
+        await connectDB()
+
+        const { name, isActive } = await req.json()
+        const slug = generateSLUG(name)
+
+        const existing = await Attribute.findOne({ name })
+        if (existing) {
+            return NextResponse.json(
+                { message: 'Attribute already exists', existing },
+                { status: 409 }
+            )
+        }
+
+        const data = await Attribute.create({
+            name,
+            slug,
+            isActive,
+        })
+        return NextResponse.json(
+            { message: 'Attribute added successfully', data },
+            { status: 201 }
+        )
+    } catch (error) {
+        return NextResponse.json(
+            { message: 'Failed to add Attribute', error: error.message },
+            { status: 400 }
+        )
+    }
+}
+
+export async function GET() {
+    try {
+        await connectDB()
+        const data = await Attribute.find()
+
+        if (data.length === 0) {
+            return NextResponse.json(
+                { message: 'Attributes not found', data },
+                { status: 404 }
+            )
+        }
+
+        return NextResponse.json(
+            { message: 'Successfully fetched Attributes', data },
+            { status: 200 }
+        )
+    } catch (error) {
+        return NextResponse.json(
+            { message: 'Failed to fetch Attributes', error: error.message },
+            { status: 400 }
+        )
+    }
+}
